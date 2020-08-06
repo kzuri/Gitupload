71d8b9f48ee57a14dc2eb1fd42a1472e26b84c94
pateldevarsh1206@gmail.com <pateldevarsh1206@gmail.com>
2020-08-06 20:54:31 +0550

    Add images

--- commit.py
+++ commit.py
@@ -1,5 +1,12 @@
-import subprocess
-print(subprocess.check_output('git init', shell=True))
-#Initialized empty Git repository in /home/code/.git/
-print(subprocess.check_output('git add .', shell=True))
-print(subprocess.check_output('git commit -m "a commit"', shell=True))
+import subprocess as cmd
+
+cmd.check_output("git add .",shell=True)
+
+response = input("Do you want to use the default message for this commit?([y]/n)\n")
+message = "update the repository"
+
+if response.startswith('n'):
+    message = input("What message you want?\n")
+
+cmd.check_output(f"git commit -m '{message}'",shell=True)
+cmd.check_output("git push -u origin master -f", shell=True)
