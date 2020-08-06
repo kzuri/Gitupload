01ee79d209613aa47c246cc23edc5a954b537ee5
pateldevarsh1206@gmail.com <pateldevarsh1206@gmail.com>
2020-08-06 20:28:01 +0530

    a commit

--- commit.py
+++ commit.py
@@ -1,13 +1,5 @@
-import subprocess as cmd
-
-cp = cmd.run("git add .", check=True, shell=True)
-#print(cp)
-
-response = input("Do you want to use the default message for this commit?([y]/n)\n")
-message = "update the repository"
-
-if response.startswith('n'):
-    message = input("What message you want?\n")
-
-cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
-cp = cmd.run("git push -u origin master -f", check=True, shell=True)
+import subprocess
+print(subprocess.check_output('git init', shell=True))
+#Initialized empty Git repository in /home/code/.git/
+print(subprocess.check_output('git add .', shell=True))
+print(subprocess.check_output('git commit -m "a commit"', shell=True))
